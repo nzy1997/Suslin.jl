e24a41c2b5a7ff18e39059a6b4e495c45643319e
nzy1997 <zhongyini1997@gmail.com>
2026-06-16 19:22:29 +0800

    test: tighten ring wrapper coverage

--- src/core/polynomials.jl
+++ src/core/polynomials.jl
@@ -1,1 +1,1 @@
-
+# Reserved split point for polynomial helpers that the later algorithm layers build on.
--- test/internal/rings.jl
+++ test/internal/rings.jl
@@ -4,6 +4,8 @@ using Oscar
 
 @testset "suslin polynomial ring" begin
     R, vars = suslin_polynomial_ring(GF(2), ["x", "y"])
+    @test ngens(R) == 2
+    @test parent(vars[1]) === R
     @test length(vars) == 2
     @test string(vars[1]) == "x"
     @test string(vars[2]) == "y"
