3a70d7f648985c8a1e3dc1de938d3b7a5366c7d5
nzy1997 <zni573@connect.hkust-gz.edu.cn>
2026-06-29 02:27:37 +0800

    test: cover elementary factor analysis branches

--- test/expert/elementary_matrices.jl
+++ test/expert/elementary_matrices.jl
@@ -2,6 +2,28 @@ using Test
 using Suslin
 using Oscar
 
+struct _FallbackExponentEntry end
+struct _UnsupportedExponentEntry end
+
+struct _ElementaryAnalysisFactor
+    entries::Matrix{Any}
+end
+
+AbstractAlgebra.nrows(factor::_ElementaryAnalysisFactor) = size(factor.entries, 1)
+AbstractAlgebra.ncols(factor::_ElementaryAnalysisFactor) = size(factor.entries, 2)
+Base.getindex(factor::_ElementaryAnalysisFactor, row::Int, col::Int) = factor.entries[row, col]
+Base.iszero(::_FallbackExponentEntry) = false
+Base.iszero(::_UnsupportedExponentEntry) = false
+
+Oscar.exponents(value::_FallbackExponentEntry; kwargs...) =
+    throw(MethodError(Oscar.exponents, (value,)))
+Oscar.exponents(value::_UnsupportedExponentEntry; kwargs...) =
+    throw(MethodError(Oscar.exponents, (value,)))
+
+AbstractAlgebra.exponent_vectors(::_FallbackExponentEntry; kwargs...) = [[5, -7], [0, 3]]
+AbstractAlgebra.exponent_vectors(value::_UnsupportedExponentEntry; kwargs...) =
+    throw(MethodError(AbstractAlgebra.exponent_vectors, (value,)))
+
 @testset "elementary matrix" begin
     F = GF(2)
     R, (x,) = Oscar.polynomial_ring(F, ["x"])
@@ -46,4 +68,22 @@ end
     ]) == 4
     @test max_elementary_factor_monomial_degree(Matrix{Any}[]) == 0
     @test total_elementary_factor_offdiagonal_monomials(Matrix{Any}[]) == 0
+end
+
+@testset "elementary factor sequence analysis metadata fallback" begin
+    fallback_factor = _ElementaryAnalysisFactor(Any[0 _FallbackExponentEntry(); 0 0])
+
+    @test max_elementary_factor_monomial_degree([fallback_factor]) == 12
+    @test total_elementary_factor_offdiagonal_monomials([fallback_factor]) == 2
+
+    unsupported_factor = _ElementaryAnalysisFactor(Any[0 _UnsupportedExponentEntry(); 0 0])
+
+    @test_throws ArgumentError max_elementary_factor_monomial_degree([unsupported_factor])
+    @test_throws ArgumentError total_elementary_factor_offdiagonal_monomials([unsupported_factor])
+
+    R, _ = suslin_polynomial_ring(QQ, ["z"])
+    nonsquare_factor = zero_matrix(R, 2, 3)
+
+    @test_throws ArgumentError max_elementary_factor_monomial_degree([nonsquare_factor])
+    @test_throws ArgumentError total_elementary_factor_offdiagonal_monomials([nonsquare_factor])
 end
