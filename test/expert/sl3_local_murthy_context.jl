302b4dbc750655f5987fe29adf2dd86d1d92c1e8
nzy1997 <zni573@connect.hkust-gz.edu.cn>
2026-06-30 00:36:07 +0800

    test: cover Murthy context guard branches

--- test/expert/sl3_local_murthy_context.jl
+++ test/expert/sl3_local_murthy_context.jl
@@ -19,6 +19,20 @@ function _captured_error(f)
     end
 end
 
+function _local_unit_witness_like(template, unit, coefficients)
+    R = parent(unit)
+    return merge(
+        template,
+        (;
+            unit,
+            residue_unit = one(R),
+            residue_inverse = one(R),
+            residue_difference_coefficients = coefficients,
+            global_unit = is_unit(unit),
+        ),
+    )
+end
+
 @testset "Murthy local input context" begin
     include(SL3_CONTEXT_FIXTURE_PATH)
     catalog = SL3MurthyGuptaFixtureCatalog.catalog()
@@ -42,9 +56,28 @@ end
     @test ordinary_context.global_units.q0 == true
     @test ordinary_context.local_units.q0 == true
     @test Suslin.verify_sl3_local_murthy_input_context(ordinary_context)
+    @test !Suslin.verify_sl3_local_murthy_input_context((; target = nothing))
 
     unrelated_split_source = by_id["mg-split-lemma-x-square"]
     unrelated_split_witness = first(unrelated_split_source.witnesses)
+    @test Suslin._sl3_local_murthy_verify_split_witness(
+        unrelated_split_source.ring.object,
+        unrelated_split_witness,
+    )
+    split_nested_context = Suslin.sl3_local_murthy_input_context(
+        unrelated_split_source.target,
+        unrelated_split_source.variable;
+        witness = (; split = unrelated_split_witness),
+    )
+    @test split_nested_context.split_witness == unrelated_split_witness
+    @test Suslin.verify_sl3_local_murthy_input_context(split_nested_context)
+    split_direct_context = Suslin.sl3_local_murthy_input_context(
+        unrelated_split_source.target,
+        unrelated_split_source.variable;
+        witness = unrelated_split_witness,
+    )
+    @test split_direct_context.split_witness == unrelated_split_witness
+    @test Suslin.verify_sl3_local_murthy_input_context(split_direct_context)
     @test unrelated_split_source.target != ordinary.target
     wrong_split_witness_err = _captured_error(() -> Suslin.sl3_local_murthy_input_context(
         ordinary.target,
@@ -135,6 +168,108 @@ end
     @test bezout_context.local_units.branch_unit == true
     @test bezout_context.bezout_witness == bezout_witness
     @test Suslin.verify_sl3_local_murthy_input_context(bezout_context)
+
+    RU = local_nonunit.ring.object
+    u = first(gens(RU))
+    local_resultant_witness = _local_unit_witness_like(
+        bezout_witness.branch_unit_witness,
+        one(RU) + u,
+        (one(RU),),
+    )
+    normalized_resultant_witnesses = Suslin._sl3_local_murthy_normalize_witness_data(
+        (; resultant_unit_witness = local_resultant_witness),
+        (;),
+        nothing,
+        nothing,
+    )
+    @test normalized_resultant_witnesses.local_unit_witnesses.resultant ==
+        local_resultant_witness
+
+    missing_resultant_unit_witness = (;
+        p0 = bezout_witness.p0,
+        q0 = bezout_witness.q0,
+        p_prime = u * bezout_witness.p_prime,
+        q_prime = u * bezout_witness.q_prime,
+        resultant = u,
+        p_prime_degree = 0,
+        q_prime_degree = 1,
+        branch_unit = 2 * u,
+        case1_entries = (;
+            p = local_nonunit.entries.p + u * bezout_witness.q_prime,
+            q = local_nonunit.entries.q + u * bezout_witness.p_prime,
+            r = u * bezout_witness.q_prime,
+            s = u * bezout_witness.p_prime,
+        ),
+    )
+    missing_resultant_err = _captured_error(
+        () -> Suslin._sl3_local_murthy_validate_required_local_evidence(
+            RU,
+            2,
+            1,
+            u,
+            (; q0 = false, resultant = false, branch_unit = false),
+            (; q0 = false, resultant = false, branch_unit = false),
+            missing_resultant_unit_witness,
+        ),
+    )
+    @test missing_resultant_err isa ArgumentError
+    @test occursin("Bezout resultant evidence", sprint(showerror, missing_resultant_err))
+
+    missing_branch_unit_witness = (;
+        p0 = bezout_witness.p0,
+        q0 = bezout_witness.q0,
+        p_prime = bezout_witness.p_prime,
+        q_prime = bezout_witness.q_prime,
+        resultant = bezout_witness.resultant,
+        p_prime_degree = bezout_witness.p_prime_degree,
+        q_prime_degree = bezout_witness.q_prime_degree,
+        branch_unit = bezout_witness.branch_unit,
+        case1_entries = bezout_witness.case1_entries,
+    )
+    missing_branch_err = _captured_error(() -> Suslin.sl3_local_murthy_input_context(
+        local_nonunit.target,
+        local_nonunit.variable;
+        witness = missing_branch_unit_witness,
+    ))
+    @test missing_branch_err isa ArgumentError
+    @test occursin("q0 or branch_unit", sprint(showerror, missing_branch_err))
+
+    shifted_bezout_witness = merge(
+        missing_branch_unit_witness,
+        (;
+            p_prime = bezout_witness.p_prime + local_nonunit.entries.q,
+            q_prime = bezout_witness.q_prime + local_nonunit.entries.p,
+            p_prime_degree = 1,
+            q_prime_degree = 2,
+            branch_unit = bezout_witness.branch_unit,
+            case1_entries = (;
+                p = local_nonunit.entries.p + bezout_witness.q_prime + local_nonunit.entries.p,
+                q = local_nonunit.entries.q + bezout_witness.p_prime + local_nonunit.entries.q,
+                r = bezout_witness.q_prime + local_nonunit.entries.p,
+                s = bezout_witness.p_prime + local_nonunit.entries.q,
+            ),
+        ),
+    )
+    degree_guard_err = _captured_error(() -> Suslin.sl3_local_murthy_input_context(
+        local_nonunit.target,
+        local_nonunit.variable;
+        witness = shifted_bezout_witness,
+    ))
+    @test degree_guard_err isa ArgumentError
+    @test occursin("degree guards", sprint(showerror, degree_guard_err))
+
+    Runit, (T,) = Oscar.polynomial_ring(QQ, ["T"])
+    @test Suslin._sl3_local_murthy_bezout_data(
+        Runit,
+        T,
+        one(Runit),
+        zero(Runit),
+        one(Runit),
+        zero(Runit),
+        0,
+        -1,
+        nothing,
+    ) === nothing
 
     corrupted_bezout_witness = merge(
         bezout_witness,
